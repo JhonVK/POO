1a783863e8047b917876bfcc0d23723d81b61cd1
João Vitor Kauer Schuck <joaovitoremailprofissional@gmail.com>
2024-07-29 17:29:18 -0300

    update

--- Trabalho_Dungeon/src/classes/MainFrameApp.java
+++ Trabalho_Dungeon/src/classes/MainFrameApp.java
@@ -24,8 +24,8 @@ public class MainFrameApp extends JFrame implements ActionListener {
         panel.add(imagem);
 
         JLabel texto= new JLabel("Escolha Seu Herói: ");
-        texto.setBounds(210, 250, 300, 100);
-        texto.setFont(new Font("Serif", Font.ROMAN_BASELINE, 30 ));
+        texto.setBounds(45, 250, 300, 100);
+        texto.setFont(new Font("Serif", Font.TYPE1_FONT, 30 ));
         panel.add(texto);
         
         addBotoes();
@@ -41,25 +41,25 @@ public class MainFrameApp extends JFrame implements ActionListener {
         ImageIcon paladino= new ImageIcon("lib\\paladino.png");
         ImageIcon barbaro= new ImageIcon("lib\\barbaro.png");
 
-        JLabel textobotao1 = new JLabel("<html>Guerreiro:<br>Habilidade: + 50% de dano<br>Dano 10<br>Saúde 4<br>Defesa 4</html>");
-        textobotao1.setBounds(40, 390, 300, 100);
-        textobotao1.setFont(new Font("Serif", Font.ROMAN_BASELINE, 15 ));
+        JLabel textobotao1 = new JLabel("<html>Soldado:<br>Habilidade: + 50% de dano<br>Dano 10<br>Saúde 4<br>Defesa 4</html>");
+        textobotao1.setBounds(30, 390, 300, 100);
+        textobotao1.setFont(new Font("Serif", Font.TYPE1_FONT, 15 ));
         panel.add(textobotao1);
         botao1= new JButton(guerreiro);
-        botao1.setBounds(40,500,200,120);
+        botao1.setBounds(30,500,200,120);
         panel.add(botao1);
         
         JLabel textobotao2= new JLabel("<html>Paladino:<br>Habilidade: + 50% de vida<br>Dano 4<br>Saúde 8<br>Defesa 6</html>");
-        textobotao2.setBounds(240, 390, 300, 100);
-        textobotao2.setFont(new Font("Serif", Font.ROMAN_BASELINE, 15 ));
+        textobotao2.setBounds(235, 390, 300, 100);
+        textobotao2.setFont(new Font("Serif", Font.TYPE1_FONT, 15 ));
         panel.add(textobotao2);
         botao2= new JButton(paladino);
-        botao2.setBounds(240,500,200,120);
+        botao2.setBounds(235,500,200,120);
         panel.add(botao2);
 
         JLabel textobotao3= new JLabel("<html>Bárbaro:<br>Habilidade: + 50% de Defesa<br>Dano 7<br>Saúde 4<br>Defesa 7</html>");
         textobotao3.setBounds(440, 390, 300, 100);
-        textobotao3.setFont(new Font("Serif", Font.ROMAN_BASELINE, 15 ));
+        textobotao3.setFont(new Font("Serif", Font.TYPE1_FONT, 15 ));
         panel.add(textobotao3);
         botao3= new JButton(barbaro);
         botao3.setBounds(440,500,200,120);
